52170b4817b8189fefe17500163104a37411331b
Shelby Kelly <shelby.kelly@colorado.edu>
2015-07-24 16:02:43 -0600

    displaying only one days events for meal planning

---
+++ app/models/event.rb
@@ -0,0 +1,61 @@
+class Event
+  class << self
+
+    def all_events(token)
+      result = api_client(token)
+
+      response = JSON.parse(result.body)
+
+      response["items"].collect { |event|
+        Event.new(event)
+      }.select { |event|
+        !event.all_day?
+      }
+    end
+
+    private
+
+    def api_client(token)
+      client = Google::APIClient.new
+      client.authorization.access_token = token
+      service = client.discovered_api('calendar', 'v3')
+      client.execute(
+        api_method: service.events.list,
+        parameters: {'calendarId' => 'primary'}
+      )
+    end
+  end
+
+  def initialize(google_event_data)
+    @google_event_data = google_event_data
+  end
+
+  def time
+    @time ||= Time.parse(@google_event_data["start"]["dateTime"])
+  end
+
+  def day
+    time.day
+  end
+
+  def hour
+    time.hour
+  end
+
+  def day_of_week
+    time.strftime("%A")
+  end
+
+  def name
+    @google_event_data["summary"]
+  end
+
+  def all_day?
+    !@google_event_data["start"]["dateTime"]
+  end
+
+  def today?
+    time.to_s.split[0] == Time.now.to_s.split[0]
+  end
+
+end
